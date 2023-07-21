4c06a10f0e791b42dbbc949b8284285c1e287db7
nikayel <alinikayeljamal@gmail.com>
2023-07-20 20:18:09 -0700

    add index.ejs and set/ route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+
 
 const app = express();
 
@@ -8,10 +11,23 @@ dotenv.config({path: "config.env"})
 const PORT = process.env.PORT || 8080;
 
 
-// app.use(morgan("tiny"));
+app.use(morgan("tiny"));
+
+
+//body bodyparser
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.set("view engine", "ejs")
+
+//load assets
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+
 
 app.get("/", function(req,res){
-  res.send("My app");
+  res.render("index.ejs");
 })
 
 
